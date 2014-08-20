14706f1ea9ba08d0f495e9803d56a66503d110c3
Alexey Buzmakov <buzmakov@gmail.com>
2014-08-20 11:37:11 +0400

    Update copyright on documentation

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'WPG'
-copyright = u'2014, Alexey Buzmakov, Liubov Samoilova'
+copyright = u'2014, Alexey Buzmakov (WPG), Liubov Samoilova (WPG), Oleg Chubar (SRWLib)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
