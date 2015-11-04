03b4c30751e4ae51a20dce486e861087c30446df
Alexey Buzmakov <buzmakov@gmail.com>
2015-11-04 17:53:48 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -258,7 +258,7 @@ try:
 except ImportError, e:
     import mock
     MOCK_MODULES = ['wpg.srwlpy','srwlpy','numpy','h5py','pylab','scipy',
-      'uti_plot', 'uti_math', 'srwl_uti_cryst', 'srwl_uti_src']
+      'uti_plot', 'uti_math', 'srwl_uti_cryst', 'srwl_uti_src', 'scipy.optimize']
     for mod_name in MOCK_MODULES:
         sys.modules[mod_name] = mock.Mock()
 else:
